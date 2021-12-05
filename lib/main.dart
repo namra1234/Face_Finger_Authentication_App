80b94d572fc0a966ee1f02a44315384b043036c9
Namra Patel <namra.patel.549@gmail.com>
2021-12-05 10:41:54 -0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/features.dart';
+
+void main() {
+  initialize();
+  runApp(MyApp());
+}
+
+void initialize() {
+  Get.lazyPut(() => AuthController());
+}
+
+class MyApp extends GetWidget<AuthController> {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Biometric Auth',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      debugShowCheckedModeBanner: false,
+      home: Obx(() {
+        if (controller.authState is UnAuthenticated) {
+          return AuthPage();
+        }else {
+          return HomePage();
+        }
+      }),
+    );
+  }
+}
